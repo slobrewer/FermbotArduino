6af3f286b7d09d2ce6240a7da3799f42feab25ca
Dave Louw <developer@slobrewer.com>
2012-08-03 21:39:39 -0700

    FERM-7: Adding new files

---
+++ src/Fermbot/Control.cpp
@@ -0,0 +1,21 @@
+/*
+ * Control.cpp
+ *
+ *  Created on: Aug 3, 2012
+ *      Author: dlouw
+ */
+
+#include "Control.h"
+
+namespace Fermbot {
+
+Control::Control() {
+   // TODO Auto-generated constructor stub
+
+}
+
+Control::~Control() {
+   // TODO Auto-generated destructor stub
+}
+
+} /* namespace Fermbot */
---
+++ src/Fermbot/Control.h
@@ -0,0 +1,20 @@
+/*
+ * Control.h
+ *
+ *  Created on: Aug 3, 2012
+ *      Author: dlouw
+ */
+
+#ifndef CONTROL_H_
+#define CONTROL_H_
+
+namespace Fermbot {
+
+class Control {
+public:
+   Control();
+   virtual ~Control();
+};
+
+} /* namespace Fermbot */
+#endif /* CONTROL_H_ */
